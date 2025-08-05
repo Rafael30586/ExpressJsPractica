8abc344a84dea4ae093663d3fdc5369c23bf78ab
Rafael305 <fraq86@gmail.com>
2025-08-05 09:30:49 -0300

    carpeta para archivos estaticos

--- node-express-course/02-express-tutorial/app.js
+++ node-express-course/02-express-tutorial/app.js
@@ -1,21 +1,19 @@
 const express = require('express')
+const path = require('path')
+
 const app = express()
 
-
+// setup static and middleware
+app.use(express.static('./public')) // Esta es la carpeta donde está el css
 
 app.get('/', (req, res) => {
-  console.log('user hit the resource')
-  res.status(200).send('Home Page')
-})
-
-app.get('/about', (req, res) => {
-  res.status(200).send('About Page')
+  res.sendFile(path.resolve(__dirname, './navbar-app/index.html'))
 })
 
 app.all('*', (req, res) => {
-  res.status(404).send('<h1>resource not found</h1>')
+  res.status(404).send('resource not found')
 })
 
 app.listen(5000, () => {
-  console.log('server is listening on port 5000...')
+  console.log('server is listening on port 5000....')
 })
